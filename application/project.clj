48440b7c1b2c1d0b795132b46b981b2bbc3c986c
Andreas Koestler <andreas.koestler@gmail.com>
2013-10-16 12:23:04 +1030

    Remove dependency for clj-time

--- application/project.clj
+++ application/project.clj
@@ -6,7 +6,6 @@
   :dependencies [[org.clojure/clojure "1.5.0"]
                  [http-kit "2.1.12"]
                  [compojure "1.1.5"]
-                 [clj-time "0.6.0"]
                  [korma "0.3.0-RC6"]
                  [org.postgresql/postgresql "9.2-1003-jdbc4"]]
   :uberjar-name "spline-webapp.jar"
