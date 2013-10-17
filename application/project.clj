d780d6b4a9a83179b9545fd0e3b7dc4f0bd8e809
Daniel Grigg <mail@danielgrigg.com>
2013-10-17 11:55:57 +1100

    just update description
    mostly so the repo pops up in my github..

--- application/project.clj
+++ application/project.clj
@@ -1,5 +1,5 @@
-(defproject spline-webapp "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+(defproject spline-webapp "0.1.0"
+  :description "webapp to capture emails"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
