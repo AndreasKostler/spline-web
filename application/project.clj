6a1968568a1be1e5be7ff8aa5a49aba4a8ec0a56
Andreas Kostler <andreas.koestler@gmail.com>
2013-10-20 08:59:01 +1030

    Handle app proberly in post-merge hoot

--- application/project.clj
+++ application/project.clj
@@ -1,6 +1,6 @@
 (defproject spline-webapp "0.1.0"
   :description "webapp to capture emails"
-  :url "http://example.com/FIXME"
+  :url "http://withflare.io"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.0"]
